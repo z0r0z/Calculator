64a9102fbd45d221990c1dd527d067d449973a98
ross <92001561+z0r0z@users.noreply.github.com>
2023-09-02 13:23:32 +0000

    🥢 Simplify comments

--- Calculator.sol
+++ Calculator.sol
@@ -1,53 +1,41 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.19;
 
-/// @title Calculator
-/// @dev Simple trustless calculator via Solidity arithmetic operators.
+/// @notice Calculator for basic arithmetic operations.
+/// @dev Simply uses only Solidity arithmetic operators.
 contract Calculator {
-    /// @notice Add two numbers.
-    /// @param a The first number.
-    /// @param b The second number.
-    /// @return The sum of a and b.
+    /// @notice Adds two numbers.
+    /// @return The sum of `a` and `b`.
     function add(uint256 a, uint256 b) public pure returns (uint256) {
         return a + b;
     }
 
-    /// @notice Subtract one number from another.
-    /// @param a The number to subtract from.
-    /// @param b The number to subtract.
-    /// @return The difference of a and b.
+    /// @notice Subtracts `b` from `a`.
+    /// @return The difference between `a` and `b`.
     function subtract(uint256 a, uint256 b) public pure returns (uint256) {
         return a - b;
     }
 
-    /// @notice Multiply two numbers.
-    /// @param a The first number.
-    /// @param b The second number.
-    /// @return The product of a and b.
+    /// @notice Multiplies two numbers.
+    /// @return The product of `a` and `b`.
     function multiply(uint256 a, uint256 b) public pure returns (uint256) {
         return a * b;
     }
 
-    /// @notice Divide one number by another.
-    /// @param a The dividend.
-    /// @param b The divisor.
-    /// @return The quotient of a divided by b.
+    /// @notice Divides `a` by `b`.
+    /// @return The quotient of `a` and `b`.
     function divide(uint256 a, uint256 b) public pure returns (uint256) {
         return a / b;
     }
 
-    /// @notice Find the remainder after dividing one number by another.
-    /// @param a The dividend.
-    /// @param b The divisor.
-    /// @return The remainder of a modulo b.
+    /// @notice Calculates the remainder of `a` divided by `b`.
+    /// @return The remainder of `a` and `b`.
     function modulo(uint256 a, uint256 b) public pure returns (uint256) {
         return a % b;
     }
 
-    /// @notice Raise a number to a power.
-    /// @param base The base number.
-    /// @param exponent The exponent to raise the base to.
-    /// @return The value of base raised to the power exponent.
+    /// @notice Raises `base` to the power of `exponent`.
+    /// @return `base` raised to the `exponent`.
     function power(uint256 base, uint256 exponent) public pure returns (uint256) {
         return base ** exponent;
     }
